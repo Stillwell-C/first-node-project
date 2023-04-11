e9c2d9eca9f99a0223f4c335b7fef8dbdd1913ad
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-04-11 17:29:55 +0900

    Change to work with express

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  let filePath = path.join(
-    __dirname,
-    "public",
-    req.url === "/" ? "index.html" : `${req.url}.html`
-  );
+const port = 8000;
+
+app.get("/", (req, res) => {
+  let filePath = path.join(__dirname, "public", "index.html");
 
   fs.readFile(filePath, (err, data) => {
     if (err) {
@@ -27,4 +27,53 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(8000);
+app.get("/about", (req, res) => {
+  let filePath = path.join(__dirname, "public", "about.html");
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      if (err.code == "ENOENT") {
+        fs.readFile(path.join(__dirname, "public", "404.html"), (err, data) => {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(data, "utf8");
+        });
+      } else {
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data, "utf8");
+    }
+  });
+});
+
+app.get("/contact-me", (req, res) => {
+  let filePath = path.join(__dirname, "public", "contact-me.html");
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      if (err.code == "ENOENT") {
+        fs.readFile(path.join(__dirname, "public", "404.html"), (err, data) => {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(data, "utf8");
+        });
+      } else {
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data, "utf8");
+    }
+  });
+});
+
+app.get("*", (req, res) => {
+  fs.readFile(path.join(__dirname, "public", "404.html"), (err, data) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data, "utf8");
+  });
+});
+
+app.listen(port);
